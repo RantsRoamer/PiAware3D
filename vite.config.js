c43342914ebecfc4bf5e5794826fd13d80faa73f
RantsRoamer <35664190+RantsRoamer@users.noreply.github.com>
2026-04-22 14:24:16 -0400

    fix: use vite-plugin-cesium@1.2.23 (correct available version)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import cesium from 'vite-plugin-cesium';
 
 export default defineConfig({
-  plugins: [],
+  plugins: [cesium()],
   server: {
     port: 5173,
     proxy: {
